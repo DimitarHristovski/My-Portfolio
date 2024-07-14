b1620e074b3b32458e6989f65e96e68405a766b8
Dimitar <dimihbt@yahoo.com>
2024-07-14 19:44:28 +0200

    Removing CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import "./Animations.css"
+import "./Animations.css";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import CardContainer from "./components/CardContainer";
-import ProjectsContainer from "./components/ProjectsContainer";
+import CardContainer from "./components/Technologies/CardContainer";
+import ProjectsContainer from "./components/Projects/ProjectsContainer";
 import About from "./components/about";
 import { ThemeProvider } from "./context/ThemeContext";
 
--- src/components/Technologies/CardContainer.tsx
+++ src/components/Technologies/CardContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from "./card";
-import { categories } from "../data/data";
+import Technologies from "./technologies";
+import { categories } from "../../data/data";
 const CardContainer: React.FC = () => {
   return (
     <div className="card-container  d-flex flex-wrap m-auto flex-column">
@@ -8,7 +8,11 @@ const CardContainer: React.FC = () => {
       <div className="row  justify-content-center ">
         {categories.map((category, index) => (
           <div className="col-lg-5  col-md-6 col-sm-12 justify-content-center">
-            <Card key={index} title={category.title} items={category.items} />
+            <Technologies
+              key={index}
+              title={category.title}
+              items={category.items}
+            />
           </div>
         ))}
       </div>
