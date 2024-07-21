a0a24d000429b2b31d9b6af05bd4f5cacbb0562d
Dimitar <dimihbt@yahoo.com>
2024-07-21 22:23:23 +0200

    responsiovness fix

--- src/components/Projects/ProjectsContainer.tsx
+++ src/components/Projects/ProjectsContainer.tsx
@@ -5,7 +5,7 @@ import { projectData } from "../../data/data";
 const ProjectsContainer = () => {
   return (
     <>
-      <div className="container-fluid py-3 col-12 hide-on-small ">
+      <div className="container-fluid py-3 col-12 d-none d-sm-block">
         <h1 className="text-center py-2">Projects Created 2021-2024</h1>
         <div className="row ">
           {projectData.map((project) => (
@@ -15,7 +15,7 @@ const ProjectsContainer = () => {
           ))}
         </div>
       </div>
-      <div className="hide-on-high text-center display-1">
+      <div className="d-sm-none d-block text-center display-1">
         Coming Soon... Check higher resolution
       </div>
     </>
